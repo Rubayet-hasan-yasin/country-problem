45947a3c352be3b60b60384cae38c538bf5b8a2b
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-03-01 11:55:37 +0600

    finish

--- countries.js
+++ countries.js
@@ -1,73 +1,136 @@
-const loadCountries = () =>{
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then(data => {displayCountries(data), dropdown(data)})
+const loadCountries = (SearchText) => {
+    const url = `https://restcountries.com/v3.1/${SearchText}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => { displayCountries(data), dropdown(data) })
 }
 
-const displayCountries = countries =>{
+const displayCountries = countries => {
     const countriesContainer = document.getElementById('all-countries');
     // for(const countrie of countries){
     //     console.log(countrie)
     // }
 
+    countriesContainer.innerHTML = '';
+
     countries.forEach(element => {
-        
+
         const countryDiv = document.createElement('div');
         countryDiv.classList.add("country");
         const name = element.name.common;
-        
+
         const capital = element.capital ? element.capital[0] : 'No Capital';
+
         
-        
-        countryDiv.innerHTML = 
-        `
+
+
+        countryDiv.innerHTML =
+            `
         <img src="${element.flags.png}" height="160" alt="">
         <h3>Name: ${name} </h3>
         <p>Capital: ${capital}</P>
+        <p>Capital: ${element.region}</P>
+        
         <button class="btn btn-primary">Details</button>
 
         `;
-        
+
         countriesContainer.appendChild(countryDiv)
-        
+
     });
 }
 
-function dropdown(data){
-    
-    const ul = document.getElementById('ul');
-    const newRegion = [];
-    data.forEach(data => {
-        
-        
-        const region = data.region
-        console.log(region)
-        newRegion.push(region)
-        ul.innerHTML = `
-    <li><a class="dropdown-item" href="#">Another action</a></li>
-    `
-    })
+function dropdown(data) {
+    const optionContainer = document.getElementById('ul-container');
+    optionContainer.innerHTML ='';
+    const option = document.createElement('option');
+    option.value = 'all';
 
-    for(const region of newRegion){
-        if(newRegion === false){
-            newRegion.push(region)
-        }
-    }
-    console.log(newRegion)
-    
-    
-    
+    option.innerHTML = `
+                        <a href="#">all</a>
+                        `
+    optionContainer.appendChild(option)
+
+    // console.log(optionContainer.childNodes)
+    // optionContainer.childNodes.innerHTML = '';
+
+
+
+
+    const newRegion = [];
+
+
+    data.forEach(data => {
+        const region = data.region
+        // console.log(region)
+        newRegion.push(region)
+
+    });
+
+    const stringset = new Set(newRegion);
+    const uniqueString = [...stringset];
+
+    // console.log(uniqueString)
+    // displayRigion(uniqueString)
+    uniqueString.forEach(data => {
+
+
+        const option = document.createElement('option');
+        option.value = data;
+
+        option.innerHTML = `
+                            <a href="#">${data}</a>
+                            `
+        optionContainer.appendChild(option)
+    });
 
 }
 
-// const displayDetails = code =>{
-//     const url = `
-//     https://restcountries.com/v3.1/alpha/${code}
-//     `
-    
-//     fetch(url)
-//     .then(res => res.json())
-//     .then(data => console.log(data))
+const loadRegionDetails = (code) => {
+    const url = `
+    https://restcountries.com/v3.1/region/${code}
+    `
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountries(data))
+        
+}
+
+// const displayRigion = region => {
+//     const li = document.getElementsByClassName('list');
+//     // console.log(li)
+
+//     for (const node of li) {
+//         console.log(node)
+//     }
+
+//     // li.forEach(data => {
+//     //     console.log(data)
+//     // })
+
 // }
 
-loadCountries()
+// displayRigion()
+document.getElementById('ul-container').addEventListener('change', function (e) {
+    if(e.target.value === 'all'){
+        loadCountries('all')
+    }
+    else{
+
+        loadRegionDetails(e.target.value);
+    }
+    console.log(e.target.value)
+    
+})
+
+// show all region 
+// document.getElementById('all-region').addEventListener('click', function(){
+//     loadCountries('all')
+// })
+loadCountries('all')
+
+
+
+
+
